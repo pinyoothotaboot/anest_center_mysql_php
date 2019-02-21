df36050bda968ac76ce7e5f815a40fd0c28e8c8a
pinyoo Thotaboot <pinyoo_too@hotmail.com>
2019-02-21 22:00:12 +0700

    Update param.php

--- config/param.php
+++ config/param.php
@@ -3,16 +3,16 @@
 	/*
 		This is for other hash keys .. Not sure yet
 	*/
-	define('HASH_GENERAL_KEY','AcEhKrBpQ%8tWh048265321');
+	define('HASH_GENERAL_KEY','AcEhKr');
 	
 	/*
 		This is for database password only
 	*/
 	define('HASH_PASSWORD_KEY','lkjshfht;iif%#$#756e23dfsdkfklsltkgdkjkitiertavsljtijSDFADSFSDGBFDKSJLDG');
 	
-	//define('USERNAME','hacked');
+	
 	define('USERNAME','cbbd8d804cf570c8201eb2dfda5737cb');
-	//define('PASSWORD','acehkrbpq%8twh048265321');
+	
 	define('PASSWORD','0043a3d87fb7f9b7ef54ce7bc4df6527');
 	
 ?>
